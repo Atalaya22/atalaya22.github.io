70cad077074dd65e9b726c94fa53720440da26a9
Aleix Ferré <thecatalahd@gmail.com>
2022-12-18 03:15:33 +0100

    Moving things around

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ErrorComponent } from './error/error.component';
-import { ProjectDetailsComponent } from './home/gallery/project-details/project-details.component';
-import { ProjectDetailsGuard } from './home/gallery/project-details/project-details.guard';
+import { ErrorComponent } from './common/error/error.component';
+import { ProjectDetailsComponent } from './gallery/project-details/project-details.component';
+import { ProjectDetailsGuard } from './gallery/project-details/project-details.guard';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { ErrorComponent } from './error/error.component';
-import { HeaderComponent } from './header/header.component';
-import { GalleryComponent } from './home/gallery/gallery.component';
+import { ErrorComponent } from './common/error/error.component';
+import { HeaderComponent } from './common/header/header.component';
+import { GalleryComponent } from './gallery/gallery.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ProjectDetailsComponent } from './home/gallery/project-details/project-details.component';
+import { ProjectDetailsComponent } from './gallery/project-details/project-details.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FooterComponent } from './common/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ErrorComponent, HeaderComponent, GalleryComponent, ProjectDetailsComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ErrorComponent,
+    HeaderComponent,
+    GalleryComponent,
+    ProjectDetailsComponent,
+    ProfileComponent,
+    FooterComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
