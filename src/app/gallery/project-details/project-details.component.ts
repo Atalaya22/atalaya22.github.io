c000129a58cca35cb0ea15b8c1a9d5a6d084fab5
Aleix Ferré <thecatalahd@gmail.com>
2022-12-27 20:46:47 +0100

    adding arrows to the gallery view

--- src/app/gallery/project-details/project-details.component.ts
+++ src/app/gallery/project-details/project-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { ProjectDetails, ProjectName, PROJECT_DETAILS } from '../gallery.model';
@@ -10,10 +10,36 @@ import { ProjectDetails, ProjectName, PROJECT_DETAILS } from '../gallery.model';
 })
 export class ProjectDetailsComponent implements OnInit {
   project!: ProjectDetails;
+  currentId = 0;
 
   constructor(@Inject(MAT_DIALOG_DATA) private data: ProjectName) {}
 
   ngOnInit() {
     this.project = PROJECT_DETAILS[this.data];
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  onKeyDown(event: KeyboardEvent) {
+    if (event.key === 'ArrowLeft') {
+      this.previous();
+    } else if (event.key === 'ArrowRight') {
+      this.next();
+    }
+  }
+
+  canGoPrevious(): boolean {
+    return this.currentId !== 0;
+  }
+
+  canGoNext(): boolean {
+    return this.currentId !== this.project.nImages - 1;
+  }
+
+  next() {
+    if (this.canGoNext()) this.currentId += 1;
+  }
+
+  previous() {
+    if (this.canGoPrevious()) this.currentId -= 1;
   }
 }
